c2636d5b5897a9e56b7d4bf1f4f50c249627b5d1
Kavyashree Chandrashekar <kavyashree@vitalexchange.com>
2020-10-27 17:55:41 -0700

    Navigation drawer on user Home

--- ToyTrader/app/src/main/java/com/example/toytrader/RegisterActivity.java
+++ ToyTrader/app/src/main/java/com/example/toytrader/RegisterActivity.java
@@ -1,20 +1,24 @@
 package com.example.toytrader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextView loginText;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         onClickLoginText();
+        onClickRegisterButton();
     }
 
     public void onClickLoginText(){
@@ -23,6 +27,17 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(".LoginActivity");
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onClickRegisterButton(){
+        registerButton =(Button)findViewById(R.id.register_btn);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(".UserHomeActivity");
                 startActivity(intent);
             }
         });
