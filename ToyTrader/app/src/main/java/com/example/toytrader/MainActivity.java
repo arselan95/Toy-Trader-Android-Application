52964cbf108274a4db81a9a9b22e9c476912b1bf
Kavyashree Chandrashekar <kavyashree@vitalexchange.com>
2020-10-29 16:31:00 -0700

    Start Page and Upload toy in navigation drawer

--- ToyTrader/app/src/main/java/com/example/toytrader/MainActivity.java
+++ ToyTrader/app/src/main/java/com/example/toytrader/MainActivity.java
@@ -6,37 +6,17 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private Button loginButton;
     private Button signUpButton;
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.action_bar);
-        setSupportActionBar(toolbar);
         onClickLoginButton();
         onClickRegisterButton();
     }
@@ -62,11 +42,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //add an employee
-    public void uploadToy(View view) {
-        //new DbManager (this);
-        Intent intent;
-        intent = new Intent(this, UploadToy.class);
-        startActivity(intent);
-    }
 }
